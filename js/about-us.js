e644dfebeb8a93dc25e09a79e7ea3c990492bb9c
MrBushido21 <123465593+MrBushido21@users.noreply.github.com>
2023-06-08 14:04:15 +0300

    Update about-us.js

--- js/about-us.js
+++ js/about-us.js
@@ -21,7 +21,7 @@ window.addEventListener('scroll', function() {
   //================================================================================================================================================
   //-----------------TEXT---------------\
 const historyThumbs = new Swiper('.history__thumbs', {
-
+  slidesPerView: 9,
 });
 const historyTop = new Swiper('.history-top', {
   spaceBetween: 10,
@@ -31,5 +31,27 @@ const historyTop = new Swiper('.history-top', {
   },
   thumbs: {
     swiper: historyThumbs
+  }
+});
+//================================================================================================================================================
+//-----------------TEXT---------------\
+const label = document.querySelector('.form-label');
+const form = document.querySelector('.form__form');
+
+label.addEventListener('click', function() {
+  if (!label.classList.contains('active')) {
+    label.classList.add('active');
+  } else {
+    label.classList.remove('active');
+  } 
+});
+
+form.addEventListener('submit', function(event) {
+  if(!label.classList.contains('active')) {
+    event.preventDefault()
+    label.classList.add('error');
+    alert('Ви маєте погодитися із правилами!');
+  } else {
+    label.classList.remove('error');
   }
 });
